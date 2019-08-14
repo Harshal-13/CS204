abcf6cfa5bf012b24769c3147d833e74bc87cb12
Harshal-13 <51031520+Harshal-13@users.noreply.github.com>
2019-08-14 16:09:59 +0530

    Add files via upload

---
+++ lab3_brackets.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool isBalanced(string expr)
+{
+	stack<char> s;
+	char x;
+  if(expr[0]!='(' && expr[0]!='[' && expr[0]!='{' && expr[0]!='<' && expr[0]!='|') return false;
+
+	for(int i = 0; i < expr.length(); i++)
+	{
+		if(expr[i] == '(' || expr[i] == '{' || expr[i] == '[' || expr[i] == '<' || expr[i] == '|')
+		{
+      if(s.size()==0){
+        s.push(expr[i]);
+        continue;
+      }
+      if(expr[i] == '|' && i>0){
+        if(s.top()=='|'){
+          s.pop();
+        }
+        else s.push(expr[i]);
+      }
+			else s.push(expr[i]);
+		}
+
+    if(s.empty() && i!= expr.length()-1)
+			return false;
+
+    switch(expr[i])
+		{
+			case ')':
+				x = s.top();
+				s.pop();
+				if(x != '(')
+					return false;
+				break;
+			case '}':
+				x = s.top();
+				s.pop();
+				if(x != '{')
+					return false;
+				break;
+			case ']':
+				x = s.top();
+				s.pop();
+				if(x != '[')
+					return false;
+				break;
+			case '>':
+				x = s.top();
+				s.pop();
+				if(x != '<')
+					return false;
+				break;
+
+		}
+	}
+	return (s.empty());
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	string expr;
+	while(n--)
+	{
+		cin >> expr;
+		if(isBalanced(expr))
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+	}
+	return 0;
+}
