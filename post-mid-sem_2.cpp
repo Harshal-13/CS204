6bb2e9745eed6d40db41d32ab957eee8e7b20261
Harshal-13 <51031520+Harshal-13@users.noreply.github.com>
2019-10-04 00:27:14 +0530

    Update post-mid-sem_2.cpp

--- post-mid-sem_2.cpp
+++ post-mid-sem_2.cpp
@@ -3,115 +3,121 @@ using namespace std;
 
 struct Point
 {
-    int x, y;
+	int x, y;
 };
 
-Point p0;
+Point P0;
 
-Point nextToTop(stack<Point> &choco)
+
+
+void swap_by_pointers(Point &p1, Point &p2)
 {
-    Point p = choco.top();
-    choco.pop();
-    Point res = choco.top();
-    choco.push(p);
-    return res;
+	Point temp=p1;
+	p1=p2;
+	p2=temp;
 }
 
-void swap(Point &p1, Point &p2)
+int Dist_Square(Point p1, Point p2)
 {
-    Point temp = p1;
-    p1 = p2;
-    p2 = temp;
-}
-
-int distSq(Point p1, Point p2)
-{
-    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+	int ans=(p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y);
+	return ans;
 }
 
 int orientation(Point p, Point q, Point r)
 {
-    int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
-
-    if (val == 0)
-        return 0;
-    return (val > 0) ? 1 : 2;
+	int value=(q.y-p.y)*(r.x-q.x)-(q.x-p.x)*(r.y-q.y);
+	if(value==0)return 0; //colinear
+	else if(value>0)return 2; //clockwise
+	return 1; //anticlockwise
 }
 
 int compare(const void *vp1, const void *vp2)
 {
-    Point *p1 = (Point *)vp1;
-    Point *p2 = (Point *)vp2;
+	Point *p1=(Point *)vp1;
+	Point *p2=(Point *)vp2;
 
-    int o = orientation(p0, *p1, *p2);
-    if (o == 0)
-        return (distSq(p0, *p2) >= distSq(p0, *p1)) ? -1 : 1;
-
-    return (o == 1) ? -1 : 1;
+	int orient=orientation(P0, *p1, *p2);
+	if(orient==0)
+	{
+		if(Dist_Square(P0, *p2)>=Dist_Square(P0, *p1))return -1;
+		else return 1;
+	}
+	if(orient==2) return -1;
+	return 1;
+}
+Point Next_to_Top_in_stack(stack <Point> &s)
+{
+	Point p=s.top();
+	s.pop();
+	Point answer=s.top();
+	s.push(p);
+	return answer;
 }
 
-void Hull(Point coordinates[], int n)
+void Convex_Hull(Point pt[], int n)
 {
-    int xmin = coordinates[0].y, min = 0;
-    for (int i = 1; i < n; i++)
-    {
-        int x = coordinates[i].x;
+	int x_min=pt[0].x;
+	int min=0;
+	for(int i=1;i<n;++i)
+	{
+		if( pt[i].x<x_min || (pt[i].x==x_min && pt[i].y<pt[min].y) )
+			{
+				min=i;
+				x_min=pt[i].x;
+			}
+	}
 
-        if (x < xmin || (xmin == x && coordinates[i].y < coordinates[min].y))
-            xmin = coordinates[i].x, min = i;
-    }
+	swap_by_pointers(pt[0], pt[min]);
 
-    swap(coordinates[0], coordinates[min]);
+	P0=pt[0];
+	qsort(&pt[1], n-1, sizeof(Point), compare);
+	int m=1;
+	for(int i=1;i<n;++i)
+	{
+		while(i<n-1 && orientation(P0, pt[i], pt[i+1])==0)
+			i++;
+		pt[m]=pt[i];
+		m++;
+	}
 
-    p0 = coordinates[0];
-    qsort(&coordinates[1], n - 1, sizeof(Point), compare);
+	if(m<3)
+	{
+		cout<<"Convex Hull not possible"<<endl;
+		return;
+	}
 
-    int m = 1;
-    for (int i = 1; i < n; i++)
-    {
-        while (i < n - 1 && orientation(p0, coordinates[i], coordinates[i + 1]) == 0)
-            i++;
+	stack <Point> s;
+	s.push(P0);
+	s.push(pt[1]);
+	s.push(pt[2]);
 
-        coordinates[m] = coordinates[i];
-        m++;
-    }
-    if (m < 3)
-        return;
-
-    stack<Point> choco;
-    choco.push(coordinates[0]);
-    choco.push(coordinates[1]);
-    choco.push(coordinates[2]);
-
-    for (int i = 3; i < m; i++)
-    {
-        while (orientation(nextToTop(choco), choco.top(), coordinates[i]) != 1)
-            choco.pop();
-        choco.push(coordinates[i]);
-    }
-    Point a[m];
-    int i = 0;
-    while (!choco.empty())
-    {
-        Point p = choco.top();
-        a[i].x = p.x;
-        a[i].y = p.y;
-        choco.pop();
-        i++;
-    }
-    cout << i << endl;
-    for (int j = m - 1; j >= 0; j--)
-        cout << a[j].x << " " << a[j].y << endl;
+	for(int i=3;i<m;++i)
+	{
+		while(orientation(Next_to_Top_in_stack(s), s.top(), pt[i])!=2)
+			s.pop();
+		s.push(pt[i]);
+	}
+	cout<<s.size()<<endl;
+	vector <Point> ans;
+	while(!s.empty())
+	{
+		Point p=s.top();
+		ans.push_back(p);
+		s.pop();
+	}
+	for(int i=ans.size()-1;i>=0;--i)
+		cout<<ans[i].x<<" "<<ans[i].y<<endl;
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    Point a[n];
-    for (int i = 0; i < n; i++)
-        cin >> a[i].x >> a[i].y;
-
-    Hull(a, n);
-    return 0;
+	int n;
+	cin>>n;
+	Point p[n];
+	for(int i=0;i<n;++i)
+	{
+		cin>>p[i].x>>p[i].y;
+	}
+	Convex_Hull(p, n);
+	return 0;
 }
